29ce95af6c771731ea7e5c98c630699f29567fd4
abula28 <nikaabuladze28@gmail.com>
2023-08-29 12:26:09 +0500

    get put delete update

--- Backend/app.js
+++ Backend/app.js
@@ -41,6 +41,68 @@ app.post('/', (req, res) => {
   );
 });
 
+app.put('/:id', (req, res) => {
+  const tourId = req.params.id * 1;
+  const updatedTour = req.body;
+  const tourIndex = tours.findIndex((tour) => tour.id === tourId);
+
+  if (tourIndex === -1) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Tour not found',
+    });
+  }
+
+  tours[tourIndex] = { ...tours[tourIndex], ...updatedTour };
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        return res.status(500).json({
+          status: 'error',
+          message: 'Could not update tour',
+        });
+      }
+      res.status(200).json({
+        status: 'success',
+        data: {
+          tour: tours[tourIndex],
+        },
+      });
+    }
+  );
+});
+
+app.delete('/:id', (req, res) => {
+  const tourId = req.params.id * 1;
+  const tourIndex = tours.findIndex((tour) => tour.id === tourId);
+
+  if (tourIndex === -1) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Tour not found',
+    });
+  }
+  tours.splice(tourIndex, 1);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        return res.status(500).json({
+          status: 'error',
+          message: 'Could not delete tour',
+        });
+      }
+      res.status(204).json({
+        status: 'success',
+        data: null,
+      });
+    }
+  );
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,32 +3,59 @@ import "./App.css";
 import axios from "axios";
 
 const apiURL = "http://localhost:3000";
+
 function App() {
   const [data, setData] = useState([]);
   const [value, setValue] = useState("");
+  const [editingId, setEditingId] = useState();
+  const [editingName, setEditingName] = useState("");
   useEffect(() => {
     getData();
   }, []);
 
-  const getData = () => {
-    axios
-      .get(apiURL)
-      .then((resp) => {
-        setData(resp.data.data.tours);
-        console.log(resp.data.data.tours);
-      })
-      .catch((err) => {
-        console.log(err);
+  const getData = async () => {
+    try {
+      const response = await axios.get(apiURL);
+      setData(response.data.data.tours);
+      console.log(response.data.data.tours);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleUpdate = async (id) => {
+    try {
+      await axios.put(`${apiURL}/${id}`, {
+        name: editingName,
       });
+      await getData();
+      setEditingId(null);
+      setEditingName("");
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    axios.post(apiURL, {
-      name: value,
-    });
-    getData();
-    setValue("");
+    try {
+      await axios.post(apiURL, {
+        name: value,
+      });
+      await getData();
+      setValue("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`${apiURL}/${id}`);
+      await getData();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -40,13 +67,26 @@ function App() {
           type="text"
         />
       </form>
-      {data.map((e) => {
-        return (
-          <div key={e.id}>
-            <p>{e.name}</p>
-          </div>
-        );
-      })}
+      {data.map((e) => (
+        <div key={e.id}>
+          {editingId === e.id ? (
+            <>
+              <input
+                value={editingName}
+                onChange={(e) => setEditingName(e.target.value)}
+                type="text"
+              />
+              <button onClick={(event) => handleUpdate(e.id)}>Update</button>
+            </>
+          ) : (
+            <>
+              <p>{e.name}</p>
+              <button onClick={() => setEditingId(e.id)}>Edit</button>
+              <button onClick={() => handleDelete(e.id)}>Delete</button>
+            </>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
